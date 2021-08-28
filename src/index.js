291ab6cdce9fbba96dcfa0ed508727cdbf572ab7
nelsoneo <nelsoneo@gmail.com>
2021-08-28 20:30:45 -0300

    subindo projeto

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function soma(a, b) {
+  return a + b;
+}
+
+
+function primeiroJSX() {
+  return (
+     <div className="test">
+       Nelson Hernandez - Curso de React JS
+       <h1>Soma: {soma(10, 20)}</h1>
+     </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div className= "App">
+      {primeiroJSX()}
+    </div>
+  )
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
